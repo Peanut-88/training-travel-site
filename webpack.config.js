9be61119657ad2961bb7516c77c1fa2f1bd14531
Rachid <ahmed-rachid@outlook.com>
2020-11-01 18:18:52 +0100

    Completed our build process configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,20 @@
+const currentTask = process.env.npm_lifecycle_event
+
 // package de la librairie node
 // permet peut importe le system d'exploitation (windows mac ou linux)
 // de générer le chemin absolue du bon dossier
 const path = require('path') 
+
+// import du plugin npm webpack pour gérer la suppression
+const {CleanWebpackPlugin } = require ('clean-webpack-plugin')
+
+// permet d'extraire fichier CSS du bundle.js
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+//  
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const fse = require('fs-extra')
 
 const postCSSPlugin = [
     require('postcss-import'),
@@ -12,46 +25,98 @@ const postCSSPlugin = [
     require('autoprefixer')
 ]
 
-module.exports = { // on export l'objet qui doit être utilisé par webpack
-    // point d'entrée de notre application
-    // fichier que webpack doit surveiller traiter et "bundle"
+// afin de copier les images
+class RunAfterCompile {
+    apply(compiler) {
+        compiler.hooks.done.tap('Copy images', function() {
+            fse.copySync('./app/assets/images', './docs/assets/images')
+        })
+    }
+}
+
+let cssConfig = {
+    test: /\.css$/i,
+    use: ['css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugin }}]
+}
+
+// on veut récupérer un array contenant tous les fichiers html
+let pages = fse.readdirSync('./app').filter(function(file) {
+    return file.endsWith('.html')
+}).map(function(page) {
+    return new HtmlWebpackPlugin({
+        filename: page,
+        template: `./app/${page}`
+    })
+})
+
+let config = {
     entry: './app/assets/scripts/App.js',
-    output: {
-        // on nomme le "bundle file"
-        filename: 'bundle.js',
-        // où se situe le fichier
-        path: path.resolve(__dirname, 'app')
-    },
-    devServer: {
-        before: function(app, server) {
-            server._watch('./**/*.html') // ** : n'importe quel sous dossier, *.html: nimporte quel fichier finissant par html
-        },
-        contentBase: path.join(__dirname, 'app'),// dossier du server: ici app car dans contient index.html
-        hot: true, // permet injection CSS et HTML sans refresh
-        port: 3000,
-        host: '0.0.0.0' // permet au appareil sur le meme réseau d'acceder au server webpack
-    },
-    // mode developement car on est pas prêt de livrer fichier
-    // où de les placer sur serveur d'hebergement
-    mode: 'development',
-    // warch : surveiller les modifications des fichiers sources (ou entry file)
-    // et relancer le "bundle"
-    // watch: true, // plus besoin du watch car devServer s'en charge
-    // Faire en sorte que Webpack supporte le CSS 
+    /*plugins: [new HtmlWebpackPlugin({filename: 'index.html', template: './app/index.html'})],*/
+    plugins: pages,
     module: {
         rules: [
-            {
-                // on ne s'intersse (test) qu'aux fichier se terminant pas /css
-                test: /\.css$/i,
-                // on va dire ce qu'on fait de ces fichiers
-                // en utilisant les packages npm installés
-                // on utilise le css-loader : pour que webpack comprenne et bundle le css
-                // et style-loader : pour appliquer ce CSS au navigateur
-                // dernier parametre : permet d'utiliser post-css
-                //      option = option que l'on souhaite utiliser
-                use: ['style-loader', 'css-loader?url=false', 
-                    {loader: 'postcss-loader', options: {plugins: postCSSPlugin }}]
-            }
+            cssConfig
         ]
     }
 }
+
+if (currentTask == "dev") {
+    // unshift pour ajouter element debut array
+    cssConfig.use.unshift('style-loader')
+
+    config.output = {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'app')
+    }
+    config.devServer = {
+        before: function(app, server) {
+            server._watch('./**/*.html') 
+        },
+        contentBase: path.join(__dirname, 'app'),
+        hot: true, 
+        port: 3000,
+        host: '0.0.0.0' 
+    }
+    config.mode = 'development'
+} 
+
+if (currentTask == "build") {
+    config.module.rules.push({
+        /* on indique les règles qu'on veut appliquer pour les fichiers js*/
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env']
+            }
+        }
+    })
+
+    cssConfig.use.unshift(MiniCssExtractPlugin.loader)
+    postCSSPlugin.push(require('cssnano'))
+    
+    config.output = {
+        /* Pour créer noveau fichier avec nouveau nom en cas de modification  */
+        /*filename: 'bundle.js',*/
+        filename: '[name].[chunkhash].js',
+        chunkFilename: '[name].[chunkhash].js',
+        path: path.resolve(__dirname, 'docs')
+    }
+    config.mode = 'production'
+    
+    /* permet de créer fichier vendor */
+    config.optimization = {
+        splitChunks: {chunks: 'all'}
+    }
+    /* Import de différent plugin,
+    ici un  cleanwebpackplugin pour gérer suppression
+    lors de la génération fichiers */
+    config.plugins.push(
+        new CleanWebpackPlugin(), 
+        new MiniCssExtractPlugin({filename: 'styles.[chunkhash].css'}),
+        new RunAfterCompile())
+} 
+
+
+module.exports = config
